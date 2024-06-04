dd5ffdf8ff6baba5417d443d35b267b2c066ee29
cmc-cloud <hung.nd2@cmctelecom.vn>
2024-06-04 20:40:29 +0700

    add debug enviroment

--- utils.go
+++ utils.go
@@ -7,26 +7,6 @@ import (
 	"os"
 )
 
-// Logo log object
-func Logo(pre string, object interface{}) {
-	f, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Println(err)
-	}
-	defer f.Close()
-
-	logger := log.New(f, "", log.LstdFlags)
-	// logger.Println(object)
-	jsonString, err := json.Marshal(object)
-	if err != nil {
-		logger.Println("Error:", err)
-		return
-	}
-
-	// Print JSON string
-	logger.Println(pre + string(jsonString))
-}
-
 func convert2JsonString(object interface{}) string {
 	jsonString, err := json.Marshal(object)
 	if err != nil {
@@ -36,9 +16,60 @@ func convert2JsonString(object interface{}) string {
 	return string(jsonString)
 }
 
+// Logo log object
+func Logo(pre string, object interface{}) {
+	str, ok := os.LookupEnv("DEBUG_CMCCLOUD_TERRAFORM") // os.Getenv("DEBUG_CMCCLOUD_TERRAFORM")
+	logfile, okLogFile := os.LookupEnv("DEBUG_CMCCLOUD_LOGFILE")
+
+	if ok && str == "DEBUG" {
+		if !okLogFile {
+			logfile = "log.txt"
+		}
+		f, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Println(err)
+		}
+		defer f.Close()
+
+		logger := log.New(f, "", log.LstdFlags)
+		// logger.Println(object)
+		jsonString, err := json.Marshal(object)
+		if err != nil {
+			logger.Println("Error:", err)
+			return
+		}
+
+		// Print JSON string
+		logger.Println(pre + string(jsonString))
+	}
+}
+
 // Logs log string
 func Logs(message string) {
-	f, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	str, ok := os.LookupEnv("DEBUG_CMCCLOUD_TERRAFORM")
+	logfile, okLogFile := os.LookupEnv("DEBUG_CMCCLOUD_LOGFILE")
+
+	// Logall("str DEBUG_CMCCLOUD_TERRAFORM = " + str)
+	if ok && str == "DEBUG" {
+		if !okLogFile {
+			logfile = "log.txt"
+		}
+		// Logall("str DEBUG_CMCCLOUD_LOGFILE = " + logfile)
+		f, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Println(err)
+		}
+		defer f.Close()
+
+		logger := log.New(f, "", log.LstdFlags)
+		logger.Println(message)
+	}
+}
+
+// Logs log string
+func Logall(message string) {
+	logfile := "log.txt"
+	f, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Println(err)
 	}
